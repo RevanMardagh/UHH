22eea9a4a76c01fc84efda43bb6126b57a4da9fe
Mardagh <revan.rehman738@gmail.com>
2021-11-20 12:58:48 +0400

    added new link for law lol

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// // Buttons change menu
-// let buttons = document.getElementsByClassName("changemenu");
+// Buttons change menu
+let button1 = document.querySelectorAll(".changemenu")[0];
+let button2 = document.querySelectorAll(".changemenu")[1];
+let button3 = document.querySelectorAll(".changemenu")[2];
+let button4 = document.querySelectorAll(".changemenu")[3];
+let button5 = document.querySelectorAll(".changemenu")[4];
 
-// function menuChange() {
-//   buttons.forEach(
-//     addEventListener("click", () => {
-//       // Change class
-//       document.getElementById("burger-toggle").checked = false;
-//     })
-//   );
-// }
+function menuChange(btn) {
+  btn.addEventListener("click", () => {
+    // Change class
+    document.getElementById("burger-toggle").checked = false;
+  });
+}
 
-// console.log(buttons);
-// menuChange();
+menuChange(button1);
+menuChange(button2);
+menuChange(button3);
+menuChange(button4);
+menuChange(button5);
